68c916b024e539525a8fe911550f82f9bfa976dc
ripzimfor <Navid0022pe@gmail.com>
2025-03-09 19:29:47 +0330

    master

---
+++ t.daneshgah.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t.daneshgah._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double r;
+        
+            Console.WriteLine("Please Enter Shoaa");
+            r = Convert.ToDouble(Console.ReadLine());
+            double masahat;
+            masahat = 3.14 * (r * r);
+            Console.WriteLine("masahat daiere shoma {0} ast",masahat);
+            double mohit;
+            mohit = r * 2 * (3.14);
+            Console.WriteLine("mohit daiere shoma {0} ast", mohit);
+            Console.ReadKey();
+
+        }
+    }
+}
