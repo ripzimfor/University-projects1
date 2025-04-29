756fdaf91a61baaec8853c1243ea25d34c2b1b6e
ripzimfor <Navid0022pe@gmail.com>
2025-04-29 22:06:16 +0330

    تمرینات جدید 1404/02/9

---
+++ T.daneshgah.8/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // تعریف آرایه برای ذخیره 100 عدد
+        int[] numbers = new int[10];
+
+        // دریافت اعداد از کاربر
+        Console.WriteLine("Lotfan 10 adad vared konid:");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write($"Adad  {i + 1}:  ra vared konid ");
+            numbers[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        // جدا کردن اعداد زوج و فرد
+        int[] evenNumbers = new int[10]; // حداکثر ممکن همه زوج باشند
+        int[] oddNumbers = new int[10];   // حداکثر ممکن همه فرد باشند
+
+        int evenCount = 0;
+        int oddCount = 0;
+
+        foreach (int num in numbers)
+        {
+            if (num % 2 == 0)
+            {
+                evenNumbers[evenCount] = num;
+                evenCount++;
+            }
+            else
+            {
+                oddNumbers[oddCount] = num;
+                oddCount++;
+            }
+        }
+
+        // نمایش نتایج
+        Console.WriteLine("\nEVEN NUMBERS:");
+        for (int i = 0; i < evenCount; i++)
+        {
+            Console.Write(evenNumbers[i] + " ");
+        }
+
+        Console.WriteLine("\n\nODD NUMBERS:");
+        for (int i = 0; i < oddCount; i++)
+        {
+            Console.Write(oddNumbers[i] + " ");
+        }
+
+        Console.WriteLine("\n\nEVEN NUMBERS: " + evenCount);
+        Console.WriteLine("ODD NUMBERS: " + oddCount);
+        Console.ReadKey();
+    }
+}
---
+++ T.daneshgah.9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // تعریف آرایه برای ذخیره 5 عدد
+        int[] numbers = new int[5];
+        bool hasTwo = false;
+
+        // دریافت اعداد از کاربر
+        Console.WriteLine("lotfan 5 adad vared konid:");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write($"adad {i+1} ra vared konid: ");
+            numbers[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        // بررسی وجود عدد 2 و موقعیت آن
+        Console.WriteLine("\nnatijeh:");
+        for (int i = 0; i < 5; i++)
+        {
+            if (numbers[i] == 2)
+            {
+                hasTwo = true;
+                Console.WriteLine($"adad 2 dar mogheiat {i+1} vojoud darad.");
+            }
+        }
+
+        if (!hasTwo)
+        {
+            Console.WriteLine("adad 2 dar bein adad vared shode vojoud nadarad.");
+        }
+        Console.ReadKey();
+    }  
+}
