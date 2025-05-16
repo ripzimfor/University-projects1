be30909d3b76d9f9063b240f6ac293475b43290d
Navid peykarsetan <Navid0022pe@gmail.com>
2025-05-16 09:31:34 +0330

    Add files via upload

---
+++ T.daneshgah.10.Method mohit masahat mosalas/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T.daneshgah._10.Method_mohit_masahat_mosalas
+{
+    internal class Program
+    {
+        static double MohitTriangle(Double a , Double b , Double c)
+        {
+            return a+b+c;
+        }
+        static double MasahatTriangle(Double a, Double b, Double c)
+        {
+            double s = (a + b + c) / 2;
+            double Masahat = Math.Sqrt(s*(s-a)*(s-b)*(s-c));
+            return Masahat;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mohit,masahat Triangle application!!!");
+
+            Console.WriteLine("Please Enter First Side!!");
+            double a =Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please Enter Second Side!!");
+            Double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please Enter Third Side!!");
+            double c = Convert.ToDouble(Console.ReadLine());
+            if (a + b > c && a + c > b && c + b > a)
+            {
+                double Mohit=MohitTriangle(a, b, c);
+                double Masahat = MasahatTriangle(a, b,c);
+                Console.WriteLine($"Your triangles mohit is {Mohit}");
+                Console.WriteLine($"Your triangles Masahat is {Masahat}");
+            }else
+            {
+                Console.WriteLine("the entered sides isnt create a triangle!!!!!!!!!!!!!!!!!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ T.daneshgah.11.Tabee baazgashti zarb/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T.daneshgah._11.Tabee_baazgashti_zarb
+{
+    internal class Program
+    {
+        static int Multiply(int a, int b)
+        {
+            if (b == 0)
+            {
+                return 0;
+            }
+            if (b > 0)
+            {
+                return a + Multiply(a, b - 1);
+            }else
+                return -Multiply(a,-b);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mohasebeh haselzarb 2 adad ba tabee bazgashti ");
+            Console.WriteLine();
+            Console.Write("Enter First Number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int result= Multiply(num1, num2);
+            Console.WriteLine($"Haselzarb {num1} * {num2} = {result}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
