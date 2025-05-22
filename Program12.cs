de24856813c7bdb5f4fd10367dd5c5cfac172fd9
Navid peykarsetan <Navid0022pe@gmail.com>
2025-05-22 13:25:42 +0330

    Add files via upload

---
+++ Program12.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace _5NumberFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream(@"C:/202/121.txt", FileMode.Create, FileAccess.Write);
+
+            StreamWriter sw = new StreamWriter(fs);
+
+            Console.WriteLine("Enter Five Number:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Enter a Number:");
+                string a = Console.ReadLine();
+                sw.WriteLine(a);
+            }
+            sw.Close();
+            fs.Close();
+            FileStream f1 = new FileStream(@"C:/202/121.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(f1);
+            int sum = 0;
+
+            while (!sr.EndOfStream)
+            {
+                int a = Int32.Parse(sr.ReadLine());
+
+                sum += a;
+            }
+            Console.WriteLine("Sum = " + sum);
+            f1.Close();
+        }
+    }
+}
